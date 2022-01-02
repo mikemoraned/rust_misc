7eb7effcd63480384b7d6c2cf23c649de3c18407
Mike Moran <mike@houseofmoran.com>
2022-01-02 14:47:34 +0100

    add table

--- risk/src/main.rs
+++ risk/src/main.rs
@@ -219,6 +219,13 @@ impl StrategySummary {
             defend: defend.strategy()
         }
     }
+
+    fn from_strategy(attack: Strategy, defend: Strategy) -> StrategySummary {
+        StrategySummary {
+            attack,
+            defend
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -260,6 +267,12 @@ impl Add<Losses> for LossesSummary {
     }
 }
 
+fn average_losses(losses: LossesSummary) -> String {
+    format!("A: {:0.2}, D: {:0.2}", 
+        (losses.losses.attacker as f32) / (losses.occurrences as f32),
+        (losses.losses.defender as f32) / (losses.occurrences as f32))
+}
+
 fn main() {
     use std::collections::HashMap;
 
@@ -286,5 +299,32 @@ fn main() {
             (losses.losses.attacker as f32) / (losses.occurrences as f32),
             (losses.losses.defender as f32) / (losses.occurrences as f32));
         println!("A: {:?}, D: {:?}, average losses: {}, losses: {:?}", summary.attack, summary.defend, averages, losses);
+    }
+
+    println!(
+        "{: <10}| {: <20} | {: <20} | {: <20}",
+        "", "attack: 3", "attack: 2", "attack: 1"
+    );
+    for (defend_strategy, title) in vec![(Strategy::WithTwo, "defend: 2"), (Strategy::WithOne, "defend: 1")].iter() {
+        let attack_with_three_summary 
+            = strategy_summaries.get(&StrategySummary::from_strategy(Strategy::WithThree, *defend_strategy)).unwrap();
+        let attack_with_two_summary 
+            = strategy_summaries.get(&StrategySummary::from_strategy(Strategy::WithTwo, *defend_strategy)).unwrap();
+        let attack_with_one_summary 
+            = strategy_summaries.get(&StrategySummary::from_strategy(Strategy::WithOne, *defend_strategy)).unwrap();
+        println!(
+            "{: <10}| occurrences: {: <7} | occurrences: {: <7} | occurrences: {: <7}",
+            title, 
+            attack_with_three_summary.occurrences,
+            attack_with_two_summary.occurrences,
+            attack_with_one_summary.occurrences,
+        );
+        println!(
+            "{: <10}| {: <20} | {: <20} | {: <20}",
+            "", 
+            average_losses(*attack_with_three_summary),
+            average_losses(*attack_with_two_summary),
+            average_losses(*attack_with_one_summary),
+        );
     }
 }
